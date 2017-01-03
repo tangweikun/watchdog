4316fd4bc9a12e7d5cd62ae2d4ae5d4b5e9345c4
tangweikun <819573105@qq.com>
2017-01-03 12:27:26 +0800

    :art: use react to instead of template

---
+++ imports/app/app.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+ 
+        <ul>
+          <li>twk</li>
+          <li>twk</li>
+          <li>twk</li>
+        </ul>
+      </div>
+    );
+  }
+}
