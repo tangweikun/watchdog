4e9b4ae9d1bd7cd4911f55242394b4b80f57f531
tangweikun <819573105@qq.com>
2017-01-05 09:21:53 +0800

    :sparkles: login

---
+++ src/imports/modules/login/index.jsx
@@ -0,0 +1,93 @@
+import React, { PropTypes } from 'react'
+import { Button, Form, Icon, Input } from 'antd'
+
+const FormItem = Form.Item
+
+export default Form.create()(class Login extends React.Component {
+  static propTypes = {
+    form: PropTypes.object,
+  }
+
+  getStyles() {
+    const { innerHeight } = window
+    return {
+      title: {
+        fontSize: '60px',
+        // height: '240px',
+        lineHeight: 1.33,
+      },
+      body: {
+        backgroundColor: '#1e9a82',
+        backgroundSize: 'cover',
+        height: innerHeight,
+        minHeight: innerHeight,
+        paddingTop: (innerHeight - 550) / 2,
+        margin: 'auto',
+        color: '#ffffff',
+        textAlign: 'center',
+        fontFamily: 'STHeitiSC',
+      },
+      footer: {
+        textAlign: 'center',
+      },
+      content: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        height: '550px',
+        width: '240px',
+      },
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) console.log('--->', values)
+      console.log('-')
+    })
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form
+    const styles = this.getStyles()
+    return (
+      <Form style={styles.body} className="login-form">
+        <div style={styles.content}>
+          <div style={styles.title}>
+            WatchDog
+          </div>
+          <div style={{ marginTop: '10px' }}>
+            <FormItem>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: '请输入您的手机号!' }],
+              })(
+                <Input addonBefore={<Icon type="phone" />} placeholder="手机号" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: '请输入密码!' }],
+              })(
+                <Input addonBefore={<Icon type="lock" />} type="password" placeholder="密码" />
+              )}
+            </FormItem>
+            <FormItem>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                onClick={this.handleSubmit}
+                style={{ width: '100%', backgroundColor: '#00acc2' }}
+              >
+                登录
+              </Button>
+            </FormItem>
+            <div style={styles.footer}>
+              Powered by iHealth, 2017
+            </div>
+          </div>
+        </div>
+      </Form>
+    )
+  }
+})
